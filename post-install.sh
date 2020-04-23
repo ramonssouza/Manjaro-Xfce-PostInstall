1a62d1e94170a76119ac4592d4ec40a562af68ca
Cameron-Stuart <cameronstuart861@gmail.com>
2020-04-23 22:57:23 +0100

    Added betterdiscord setup and moved configs to a config folder

--- post-install.sh
+++ post-install.sh
@@ -6,18 +6,21 @@ echo Updating graphics drivers
 sudo mhwd -a pci nonfree 0300
 echo Updating all pacman packages
 sudo pacman -Syu
-echo Installing pacaur, lightdm, lightdm webkit and lightdm settings
-sudo pacman -S --noconfirm pacaur lightdm lightdm-webkit2-greeter lightdm-settings
+echo Installing pacaur, discord, lightdm, lightdm webkit and lightdm settings
+sudo pacman -S --noconfirm pacaur discord lightdm lightdm-webkit2-greeter lightdm-settings
 echo Disable lightdm service
 sudo systemctl disable lightdm.service
 echo Re-enable lightdm service
 sudo systemctl enable lightdm.service --force
 
+# SOFTWARE SETUPS
+bash software-setup/betterdiscord-installer.sh
+
 # NON-SUDO COMMANDS
-echo Installing VS Code, Aether lightdm theme, betterdiscord dotnet sdk preview, dotnet runtime preview, google chrome
-pacaur -S --noconfirm visual-studio-code-bin lightdm-webkit-theme-aether betterdiscord dotnet-sdk-preview dotnet-runtime-preview google-chrome
+echo Installing VS Code, Aether lightdm theme, dotnet sdk preview, dotnet runtime preview, google chrome
+pacaur -S --noconfirm visual-studio-code-bin lightdm-webkit-theme-aether dotnet-sdk-preview dotnet-runtime-preview google-chrome
 
 # FINAL COMMANDS (MUST BE EXECUTED AFTER LIGHTDM THEME INSTALL)
 echo Copying config files
-sudo cp lightdm.conf /etc/lightdm/
-sudo cp xorg.conf /etc/X11/
+sudo cp configs/lightdm.conf /etc/lightdm/
+sudo cp configs/xorg.conf /etc/X11/
---
+++ software-setup/betterdiscord-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Clone betterdiscordctl
+curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
+chmod +x betterdiscordctl
+sudo mv betterdiscordctl /usr/local/bin
+
+# Run betterdiscordctl install
+betterdiscordctl install
+
+# Download betterdiscord theme from my gists
