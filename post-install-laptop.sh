e760667fe1bf47b5a25c82fea99953209fbac870
Cameron-Stuart <cameronstuart861@gmail.com>
2020-04-29 15:07:55 +0100

    Added laptop configuration

---
+++ post-install-laptop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SUDO COMMANDS
+echo Updating Kernel
+sudo mhwd-kernel -i linux56
+echo Updating all pacman packages
+sudo pacman -Syu
+echo Installing pacaur, discord, lightdm, lightdm webkit, lightdm settings, powerline and powerline fonts
+sudo pacman -S --noconfirm pacaur discord lightdm lightdm-webkit2-greeter lightdm-settings powerline powerline-fonts
+echo Disable lightdm service
+sudo systemctl disable lightdm.service
+echo Re-enable lightdm service
+sudo systemctl enable lightdm.service --force
+
+# SOFTWARE SETUPS
+bash software-setup/betterdiscord-installer.sh
+
+# NON-SUDO COMMANDS
+echo Installing VS Code, Aether lightdm theme, dotnet sdk preview, dotnet runtime preview, google chrome
+pacaur -S --noconfirm visual-studio-code-bin lightdm-webkit-theme-aether dotnet-sdk-preview dotnet-runtime-preview google-chrome
+
+# FINAL COMMANDS (MUST BE EXECUTED AFTER LIGHTDM THEME INSTALL)
+echo Copying config files
+sudo cp configs/.bashrc ~/.bashrc
+sudo cp configs/lightdm.conf /etc/lightdm/
